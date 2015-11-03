b1f0bf3d00b5961076668cb30727e2c0b9becea5
pantalo <pantaloa@gmail.com>
2015-11-03 14:18:23 +0800

    update universal build script

--- build-mac/universal.sh
+++ build-mac/universal.sh
@@ -2,15 +2,25 @@
 set -e
 
 DEP_TARGET="libetpan ios"
-DEP_TARGET_OUTPUT="${PROJECT_NAME}-ios.a"
+DEP_TARGET_OUTPUT="libetpan-ios.a"
 DEP_TARGET_INCLUDE="include"
+ADDITION_OUTPUTS=("libsasl2.a")
+IPHONEOS_OUTPUTFOLDER="${BUILD_DIR}/${CONFIGURATION}-iphoneos"
+SIMULATOR_OUTPUTFOLDER="${BUILD_DIR}/${CONFIGURATION}-iphonesimulator"
 UNIVERSAL_OUTPUTFOLDER="${BUILD_DIR}/${CONFIGURATION}-universal"
 
-xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "${DEP_TARGET}" -configuration ${CONFIGURATION} -sdk iphoneos ONLY_ACTIVE_ARCH=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
-xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "${DEP_TARGET}" -configuration ${CONFIGURATION} -sdk iphonesimulator ARCHS="i386 x86_64" VALID_ARCHS="arm64 armv7 armv7s i386 x86_64" BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+# Build for iphoneos & iphonesimulator
+xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "${DEP_TARGET}" -configuration "${CONFIGURATION}" -sdk iphoneos ONLY_ACTIVE_ARCH=NO BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "${DEP_TARGET}" -configuration "${CONFIGURATION}" -sdk iphonesimulator ARCHS="i386 x86_64" VALID_ARCHS="arm64 armv7 armv7s i386 x86_64" BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
 
+# Create fat library
 mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
+lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/${DEP_TARGET_OUTPUT}.tmp.a" "${IPHONEOS_OUTPUTFOLDER}/${DEP_TARGET_OUTPUT}" "${SIMULATOR_OUTPUTFOLDER}/${DEP_TARGET_OUTPUT}"
 
-lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/${DEP_TARGET_OUTPUT}" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${DEP_TARGET_OUTPUT}" "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${DEP_TARGET_OUTPUT}"
+# Combine all libraries
+ALL_OUTPUTS="$(printf "${IPHONEOS_OUTPUTFOLDER}/%s " ${ADDITION_OUTPUTS[@]})${UNIVERSAL_OUTPUTFOLDER}/${DEP_TARGET_OUTPUT}.tmp.a"
+./combine.sh ${ALL_OUTPUTS} "${UNIVERSAL_OUTPUTFOLDER}/${DEP_TARGET_OUTPUT}"
+rm "${UNIVERSAL_OUTPUTFOLDER}/${DEP_TARGET_OUTPUT}.tmp.a"
 
-cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${DEP_TARGET_INCLUDE}" "${UNIVERSAL_OUTPUTFOLDER}/"
+# Copy include files
+cp -R "${IPHONEOS_OUTPUTFOLDER}/${DEP_TARGET_INCLUDE}" "${UNIVERSAL_OUTPUTFOLDER}/"
